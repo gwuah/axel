9314933d8d7393331cbf1397ac48fd719aa638ed
Griffith Asare Awuah <griffith@dolphtech.com>
2018-09-09 09:16:12 +0000

    Update axel.js

--- axel.js
+++ axel.js
@@ -1,52 +1,52 @@
-(function (global, local) {
+// (function (global, local) {
     
-    "use strict";
+//     "use strict";
     
-    function Axel(name) {
-        return new Axel.init(name);
-    }
+//     function Axel(name) {
+//         return new Axel.init(name);
+//     }
     
-    Axel.init = function (name) {
-        this.name = name;
-        // check if pointer to database already exists
-        // if it does, dont create a new database else create a new database
-        this.db = JSON.parse(localStorage.getItem(name)) || []
-    };
+//     Axel.init = function (name) {
+//         this.name = name;
+//         // check if pointer to database already exists
+//         // if it does, dont create a new database else create a new database
+//         this.db = JSON.parse(localStorage.getItem(name)) || []
+//     };
     
-    Axel.prototype = {
+//     Axel.prototype = {
 
-        contains(key, value) {
-            return this.db.some(chunk => chunk[key] == value)
-        },
+//         contains(key, value) {
+//             return this.db.some(chunk => chunk[key] == value)
+//         },
 
-        updateDb() {
-            localStorage.setItem(this.name, JSON.stringify(this.db))
-        },
+//         updateDb() {
+//             localStorage.setItem(this.name, JSON.stringify(this.db))
+//         },
 
-        save: function(data, key) {
-            // key is a parameter whose value changes in any given data
-            // it could be an "id", "hash", or any unique property of a dataset
+//         save: function(data, key) {
+//             // key is a parameter whose value changes in any given data
+//             // it could be an "id", "hash", or any unique property of a dataset
 
-            if (this.contains(key, data[key])) {
-                console.log("data already exists in db");
-                return false
+//             if (this.contains(key, data[key])) {
+//                 console.log("data already exists in db");
+//                 return false
 
-            } else {
-                this.db.push(data);
-                this.updateDb();
-                console.log("data saved!")
-                return true
-            }
-        },
+//             } else {
+//                 this.db.push(data);
+//                 this.updateDb();
+//                 console.log("data saved!")
+//                 return true
+//             }
+//         },
         
-        fetchAllSaved: function() {
-            return this.db
-        }
-    };
+//         fetchAllSaved: function() {
+//             return this.db
+//         }
+//     };
     
     
     
-    Axel.init.prototype = Axel.prototype;
-    global.a$ = global.axel = Axel;
+//     Axel.init.prototype = Axel.prototype;
+//     global.a$ = global.axel = Axel;
     
-}(window, document));
+// }(window, document));
